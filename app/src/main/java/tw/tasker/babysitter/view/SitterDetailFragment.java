d4ef1efbd8a019c9fe4b76fdbef8a291eab007c5
vic <soon530@gmail.com>
2015-10-08 11:26:50 +0800

    Download sitter home image with view pager.

--- app/src/main/java/tw/tasker/babysitter/view/SitterDetailFragment.java
+++ app/src/main/java/tw/tasker/babysitter/view/SitterDetailFragment.java
@@ -1,16 +1,30 @@
 package tw.tasker.babysitter.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import tw.tasker.babysitter.Config;
@@ -67,6 +81,11 @@ public class SitterDetailFragment extends Fragment implements OnClickListener {
 
         mSkillNumber = (TextView) rootView.findViewById(R.id.skillNumber);
         mCommunityName = (TextView) rootView.findViewById(R.id.communityName);
+
+        ViewPager pager = (ViewPager) rootView.findViewById(R.id.pager);
+        pager.setAdapter(new ImageAdapter(getActivity()));
+        //pager.setCurrentItem(getArguments().getInt(Constants.Extra.IMAGE_POSITION, 0));
+        pager.setCurrentItem(0);
 
         //initData();
         return rootView;
@@ -161,6 +180,117 @@ public class SitterDetailFragment extends Fragment implements OnClickListener {
     public void onClick(View v) {
         //mListener.onSwitchToNextFragment(Config.SITTER_EDIT_PAGE);
         getActivity().finish();
+    }
+
+
+    private static class ImageAdapter extends PagerAdapter {
+
+        private static final String[] IMAGE_URLS = new String[]{
+                "https://a0.muscache.com/im/pictures/94939537/c273236b_original.jpg?aki_policy=xx_large",
+                "https://a0.muscache.com/im/pictures/94939511/df31e96c_original.jpg?aki_policy=x_large",
+                "https://a2.muscache.com/im/pictures/94939526/e7e7c5e0_original.jpg?aki_policy=x_large"
+        };
+        private final Context mContext;
+
+        private LayoutInflater inflater;
+        private DisplayImageOptions options;
+
+        ImageAdapter(Context context) {
+            mContext = context;
+            inflater = LayoutInflater.from(context);
+
+            options = new DisplayImageOptions.Builder()
+                    //.showImageForEmptyUri(R.drawable.ic_empty)
+                    //.showImageOnFail(R.drawable.ic_error)
+                    .resetViewBeforeLoading(true)
+                    .cacheOnDisk(true)
+                    .imageScaleType(ImageScaleType.EXACTLY)
+                    .bitmapConfig(Bitmap.Config.RGB_565)
+                    .considerExifParams(true)
+                    .displayer(new FadeInBitmapDisplayer(300))
+                    .build();
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return IMAGE_URLS.length;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            View imageLayout = inflater.inflate(R.layout.item_pager_image, view, false);
+            assert imageLayout != null;
+            ImageView imageView = (ImageView) imageLayout.findViewById(R.id.image);
+
+            imageView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, ImagePageActivity.class);
+                    mContext.startActivity(intent);
+                }
+            });
+
+            final ProgressBar spinner = (ProgressBar) imageLayout.findViewById(R.id.loading);
+
+            ImageLoader.getInstance().displayImage(IMAGE_URLS[position], imageView, options, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+                    spinner.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                    String message = null;
+                    switch (failReason.getType()) {
+                        case IO_ERROR:
+                            message = "Input/Output error";
+                            break;
+                        case DECODING_ERROR:
+                            message = "Image can't be decoded";
+                            break;
+                        case NETWORK_DENIED:
+                            message = "Downloads are denied";
+                            break;
+                        case OUT_OF_MEMORY:
+                            message = "Out Of Memory error";
+                            break;
+                        case UNKNOWN:
+                            message = "Unknown error";
+                            break;
+                    }
+                    Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+
+                    spinner.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    spinner.setVisibility(View.GONE);
+                }
+            });
+
+            view.addView(imageLayout, 0);
+            return imageLayout;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view.equals(object);
+        }
+
+        @Override
+        public void restoreState(Parcelable state, ClassLoader loader) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
     }
 
 }
