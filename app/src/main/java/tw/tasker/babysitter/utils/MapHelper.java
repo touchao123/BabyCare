8629ec540a6e92f211254f020e473c57e25e6291
vic <soon530@gmail.com>
2015-10-21 14:48:43 +0800

    Get location from google map.

--- app/src/main/java/tw/tasker/babysitter/utils/MapHelper.java
+++ app/src/main/java/tw/tasker/babysitter/utils/MapHelper.java
@@ -1,7 +1,80 @@
 package tw.tasker.babysitter.utils;
 
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.parse.ParseGeoPoint;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import tw.tasker.babysitter.model.GMapAddress;
+
 public class MapHelper {
+
+	public static String parseResource(Context context, int resource) throws IOException {
+		InputStream is = context.getResources().openRawResource(resource);
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			int n;
+			while ((n = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, n);
+			}
+		} finally {
+			is.close();
+		}
+
+		return writer.toString();
+	}
+
+	public static ParseGeoPoint getLocationFromGoogleMap(String address) {
+		ParseGeoPoint parseGeoPoint = new ParseGeoPoint(0.0, 0.0);
+		try {
+			String googleMapServiceUrl = "http://maps.googleapis.com/maps/api/geocode/json?address="
+					+ address + "&sensor=false&language=zh-tw";
+			URL url = new URL(googleMapServiceUrl);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoOutput(true);
+			connection.connect();
+
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(connection
+							.getInputStream()));
+
+			Gson gson = new Gson();
+			GMapAddress gMapAddress = gson.fromJson(reader, GMapAddress.class);
+
+			System.out.println("address=" + address + " ,status=" + gMapAddress.getStatus());
+
+			if (gMapAddress.getStatus().equals("OK")) {
+
+				Double lat = gMapAddress.getResults().get(0).getGeometry().getLocation().getLat();
+				Double lng = gMapAddress.getResults().get(0).getGeometry().getLocation().getLng();
+				parseGeoPoint.setLatitude(lat);
+				parseGeoPoint.setLongitude(lng);
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+		System.out.println("(lat, lng) = (" + parseGeoPoint.getLatitude() + ", " + parseGeoPoint.getLongitude() + ")");
+		return parseGeoPoint;
+	}
+
 /*
     private String getDistance(Bundle bundle) {
 
